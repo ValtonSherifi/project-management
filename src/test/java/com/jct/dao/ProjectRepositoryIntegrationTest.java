9ecc3b8ffeeaae2c8c1d947e12f60c64c9aa6b27
Valton <valtoni.gl@live.com>
2021-03-23 23:26:52 +0100

    1 method to test the application
    added junit dependency and made one simple method to test the application

---
+++ src/test/java/com/jct/dao/ProjectRepositoryIntegrationTest.java
@@ -0,0 +1,40 @@
+package com.jct.dao;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+import org.springframework.test.context.jdbc.SqlGroup;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.jct.pma.ProjectManagementApplication;
+import com.jct.pma.dao.ProjectRepository;
+import com.jct.pma.entities.Project;
+
+@ContextConfiguration(classes = ProjectManagementApplication.class)
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@SqlGroup({@Sql(executionPhase = ExecutionPhase.BEFORE_TEST_METHOD,
+scripts = {"classpath:schema.sql","classpath:data.sql"}),
+	@Sql(executionPhase = ExecutionPhase.AFTER_TEST_METHOD,
+			scripts = "classpath:drop.sql")}) 
+public class ProjectRepositoryIntegrationTest {
+	
+	@Autowired
+	ProjectRepository proRepo;
+	
+	@Test
+	public void ifNewProjectSaved_thenSuccess() {
+		Project newProject = new Project("New Test PRoject","COMPLETED","Test description");
+		
+		proRepo.save(newProject);
+		
+		assertEquals(5,proRepo.findAll().size());
+	}
+	
+}
