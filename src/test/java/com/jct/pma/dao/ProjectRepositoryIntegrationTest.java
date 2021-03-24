f3f3313b23bdce78340db8698ba7a1eb1e0410bf
Valton <valtoni.gl@live.com>
2021-03-24 19:30:57 +0100

    test changes

--- src/test/java/com/jct/pma/dao/ProjectRepositoryIntegrationTest.java
+++ src/test/java/com/jct/pma/dao/ProjectRepositoryIntegrationTest.java
@@ -1,4 +1,4 @@
-package com.jct.dao;
+package com.jct.pma.dao;
 
 import static org.junit.Assert.assertEquals;
 
@@ -6,19 +6,18 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.jct.pma.ProjectManagementApplication;
 import com.jct.pma.dao.ProjectRepository;
 import com.jct.pma.entities.Project;
 
-@ContextConfiguration(classes = ProjectManagementApplication.class)
+@SpringBootTest
 @RunWith(SpringRunner.class)
-@DataJpaTest
 @SqlGroup({@Sql(executionPhase = ExecutionPhase.BEFORE_TEST_METHOD,
 scripts = {"classpath:schema.sql","classpath:data.sql"}),
 	@Sql(executionPhase = ExecutionPhase.AFTER_TEST_METHOD,
