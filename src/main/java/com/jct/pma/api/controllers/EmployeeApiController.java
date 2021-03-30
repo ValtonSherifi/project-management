e235cddff61958781fcb10ed12e4994f09c76329
Valton <valtoni.gl@live.com>
2021-03-30 10:51:13 +0200

    empApiController & proApiController

--- src/main/java/com/jct/pma/api/controllers/EmployeeApiController.java
+++ src/main/java/com/jct/pma/api/controllers/EmployeeApiController.java
@@ -1,11 +1,11 @@
 package com.jct.pma.api.controllers;
 
-import java.util.List;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -16,67 +16,80 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jct.pma.dao.EmployeeRepository;
 import com.jct.pma.entities.Employee;
-import com.jct.pma.services.EmployeeService;
 
 @RestController
-@RequestMapping("app-api/employees")
+@RequestMapping("/app-api/employees")
 public class EmployeeApiController {
-
+	
 	@Autowired
 	EmployeeRepository empRepo;
-
+	
 	@GetMapping
-	public List<Employee> getEmployees() {
+	public Iterable<Employee> getEmployees(){
 		return empRepo.findAll();
 	}
-
+	
 	@GetMapping("/{id}")
 	public Employee getEmployeeById(@PathVariable("id") Long id) {
 		return empRepo.findById(id).get();
 	}
-
+	
 	@PostMapping(consumes = "application/json")
 	@ResponseStatus(HttpStatus.CREATED)
 	public Employee create(@RequestBody @Valid Employee employee) {
 		return empRepo.save(employee);
 	}
-
-	@PutMapping(path = "/{id}", consumes = "application/json")
+	
+	@PutMapping(consumes = "application/json")
 	@ResponseStatus(HttpStatus.OK)
 	public Employee update(@RequestBody @Valid Employee employee) {
 		return empRepo.save(employee);
 	}
-
-	@PatchMapping(path = "/{id}", consumes = "application/json")
-	public Employee partialUpdate(@PathVariable("id") Long id, @RequestBody @Valid Employee patchEmployee) {
+	
+	@PatchMapping(path="/{id}", consumes= "application/json")
+	public Employee partialUpdate(@PathVariable("id") long id, @RequestBody @Valid Employee patchEmployee) {
 		Employee emp = empRepo.findById(id).get();
-
-		if (patchEmployee.getEmail() != null) {
+		
+		if(patchEmployee.getEmail() != null) {
 			emp.setEmail(patchEmployee.getEmail());
 		}
-		if (patchEmployee.getFirstName() != null) {
+		if(patchEmployee.getFirstName() != null) {
 			emp.setFirstName(patchEmployee.getFirstName());
 		}
-		if (patchEmployee.getLastName() != null) {
+		if(patchEmployee.getLastName() != null) {
 			emp.setLastName(patchEmployee.getLastName());
 		}
-
+		
 		return empRepo.save(emp);
+		
 	}
-
+	
+	
 	@DeleteMapping("/{id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void delete(@PathVariable("id") Long id) {
 		try {
-			empRepo.deleteById(id);
-		} catch (EmptyResultDataAccessException e) {
-
+		empRepo.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e) {
+			
 		}
 	}
-
+	
+	@GetMapping(params= {"page", "size"})
+	@ResponseStatus(HttpStatus.OK)
+	public Iterable<Employee> findPaginatedEmployees(@RequestParam("page") int page, 
+											@RequestParam("size") int size){
+		
+		Pageable pageAndSize = PageRequest.of(page, size);
+		
+		return empRepo.findAll(pageAndSize);
+	}
+	
 }
