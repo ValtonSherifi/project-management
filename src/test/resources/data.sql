a477251bc94eca53ff461090c7a5218c5f8bc7a8
Valton <valtoni.gl@live.com>
2021-03-31 15:08:23 +0200

    data

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,13 +1,13 @@
 -- INSERT EMPLOYEES			
-insert into employee (employee_id, first_name, last_name, email) values (nextval('employee_seq'), 'John', 'Warton', 'warton@gmail.com');
-insert into employee (employee_id, first_name, last_name, email) values (nextval('employee_seq'), 'Mike', 'Lanister', 'lanister@gmail.com');
-insert into employee (employee_id, first_name, last_name, email) values (nextval('employee_seq'), 'Steve', 'Reeves', 'Reeves@gmail.com');
-insert into employee (employee_id, first_name, last_name, email) values (nextval('employee_seq'), 'Ronald', 'Connor', 'connor@gmail.com');
-insert into employee (employee_id, first_name, last_name, email) values (nextval('employee_seq'), 'Jim', 'Salvator', 'Sal@gmail.com');
-insert into employee (employee_id, first_name, last_name, email) values (nextval('employee_seq'), 'Peter', 'Henley', 'henley@gmail.com');
-insert into employee (employee_id, first_name, last_name, email) values (nextval('employee_seq'), 'Richard', 'Carson', 'carson@gmail.com');
-insert into employee (employee_id, first_name, last_name, email) values (nextval('employee_seq'), 'Honor', 'Miles', 'miles@gmail.com');
-insert into employee (employee_id, first_name, last_name, email) values (nextval('employee_seq'), 'Tony', 'Roggers', 'roggers@gmail.com');
+insert into employee (employee_id, first_name, last_name, email) values (nextval('employee_seq'), 'Valton', 'Sherifi', 'valtonsherifi@gmail.com');
+insert into employee (employee_id, first_name, last_name, email) values (nextval('employee_seq'), 'Fatos', 'Ajeti', 'Ajeti@gmail.com');
+insert into employee (employee_id, first_name, last_name, email) values (nextval('employee_seq'), 'Blend', 'Ismaili', 'Ismaili.B@gmail.com');
+insert into employee (employee_id, first_name, last_name, email) values (nextval('employee_seq'), 'Roland', 'Marovci', 'RMarofci@gmail.com');
+insert into employee (employee_id, first_name, last_name, email) values (nextval('employee_seq'), 'Taulant', 'Kabashi', 'KTaulant@gmail.com');
+insert into employee (employee_id, first_name, last_name, email) values (nextval('employee_seq'), 'Fisnik', 'Isaku', 'FIIsnik@gmail.com');
+insert into employee (employee_id, first_name, last_name, email) values (nextval('employee_seq'), 'Gezim', 'Syla', 'GSyla@gmail.com');
+insert into employee (employee_id, first_name, last_name, email) values (nextval('employee_seq'), 'Etrit', 'Kosumi', 'EtritK@gmail.com');
+insert into employee (employee_id, first_name, last_name, email) values (nextval('employee_seq'), 'Xhemajl', 'Bunjaku', 'XhemaBu@gmail.com');
 
 -- INSERT PROJECTS			
 insert into project (project_id, name, stage, description) values (nextval('project_seq'), 'Large Production Deploy', 'NOTSTARTED', 'This requires all hands on deck for the final deployment of the software into production');
@@ -16,11 +16,11 @@ insert into project (project_id, name, stage, description) values (nextval('proj
 insert into project (project_id, name, stage, description) values (nextval('project_seq'), 'Improve Intranet Security', 'INPROGRESS', 'With the recent data hack, the office security needs to be improved and proper security team needs to be hired for implementation');
 
 -- INSERT PROJECT_EMPLOYEE_RELATION
-insert into project_employee (employee_id, project_id) (select e.employee_id, p.project_id from employee e, project p where e.last_name ='Warton' AND p.name = 'Large Production Deploy');
-insert into project_employee (employee_id, project_id) (select e.employee_id, p.project_id from employee e, project p where e.last_name ='Warton' AND p.name = 'New Employee Budget');
-insert into project_employee (employee_id, project_id) (select e.employee_id, p.project_id from employee e, project p where e.last_name ='Warton' AND p.name = 'Office Reconstruction');
-insert into project_employee (employee_id, project_id) (select e.employee_id, p.project_id from employee e, project p where e.last_name ='Reeves' AND p.name = 'Large Production Deploy');
-insert into project_employee (employee_id, project_id) (select e.employee_id, p.project_id from employee e, project p where e.last_name ='Warton' AND p.name = 'New Employee Budget');
-insert into project_employee (employee_id, project_id) (select e.employee_id, p.project_id from employee e, project p where e.last_name ='Warton' AND p.name = 'Improve Intranet Security');
-insert into project_employee (employee_id, project_id) (select e.employee_id, p.project_id from employee e, project p where e.last_name ='Henley' AND p.name = 'Office Reconstruction');
-insert into project_employee (employee_id, project_id) (select e.employee_id, p.project_id from employee e, project p where e.last_name ='Henley' AND p.name = 'Improve Intranet Security');															
+insert into project_employee (employee_id, project_id) (select e.employee_id, p.project_id from employee e, project p where e.last_name ='Sherifi' AND p.name = 'Large Production Deploy');
+insert into project_employee (employee_id, project_id) (select e.employee_id, p.project_id from employee e, project p where e.last_name ='Sherifi' AND p.name = 'New Employee Budget');
+insert into project_employee (employee_id, project_id) (select e.employee_id, p.project_id from employee e, project p where e.last_name ='Sherifi' AND p.name = 'Office Reconstruction');
+insert into project_employee (employee_id, project_id) (select e.employee_id, p.project_id from employee e, project p where e.last_name ='Ajeti' AND p.name = 'Large Production Deploy');
+insert into project_employee (employee_id, project_id) (select e.employee_id, p.project_id from employee e, project p where e.last_name ='Sherifi' AND p.name = 'New Employee Budget');
+insert into project_employee (employee_id, project_id) (select e.employee_id, p.project_id from employee e, project p where e.last_name ='Marovci' AND p.name = 'Improve Intranet Security');
+insert into project_employee (employee_id, project_id) (select e.employee_id, p.project_id from employee e, project p where e.last_name ='Ismaili' AND p.name = 'Office Reconstruction');
+insert into project_employee (employee_id, project_id) (select e.employee_id, p.project_id from employee e, project p where e.last_name ='Kabashi' AND p.name = 'Improve Intranet Security');															
